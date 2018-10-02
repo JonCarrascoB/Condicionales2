6e4443357b73b124c2c1508010deb7fe7301a020
Jon Carrasco <jcarrascobar@gmail.com>
2018-10-02 11:34:59 +0200

    comit condicionales 2

--- Condicionales 3/Condicionales 3/Program.cs
+++ Condicionales 3/Condicionales 3/Program.cs
@@ -11,7 +11,7 @@ namespace Condicionales_3
         static void Main(string[] args)
         {
 
-            ////// introducir dos numeros e indicar si son o no iguales
+            ////// introducir dos numeros e indicar si son o no iguales. Colocar las variables siempre en el inicio por buenas practicas
             //int num1, num2;
             //Console.WriteLine("introduce un valor");
             //num1 = Convert.ToInt32(Console.ReadLine());
@@ -23,7 +23,15 @@ namespace Condicionales_3
             //Console.ReadLine();
 
 
-
+            //// Pedir un numero y decir si es par o no
+            //int num1;
+            //Console.WriteLine("introducir numero: ");
+            //num1 = Convert.ToInt32(Console.ReadLine());
+            //if (num1 % 2 == 0)
+            //{ Console.WriteLine("el numero es par"); }
+            //else
+            //{ Console.WriteLine("el numero es impar"); }
+            //Console.ReadLine();
 
 
             ////Pedir un numero del 1-7 y decir a que día de la semana corresponde
@@ -64,55 +72,74 @@ namespace Condicionales_3
             //}
             //Console.ReadLine();
 
-            // pedir un numero numero del 1-12 y que aparezca el día del mes.
-            int num1;
-            Console.WriteLine("introducir numero mes: ");
-            num1 = Convert.ToInt32(Console.ReadLine());
-            switch (num1)
-            { case 1:
-                    Console.WriteLine("el mes es Enero");
-                    break;
-                case 2:
-                    Console.WriteLine("el mes es Febrero");
-                    break;
-                case 3:
-                    Console.WriteLine("el mes es Marzo");
-                    break;
-                case 4:
-                    Console.WriteLine("el mes es Abril");
-                    break;
-                case 5:
-                    Console.WriteLine("el mes es Mayo");
-                    break;
-                case 6:
-                    Console.WriteLine("el mes es Junio");
-                    break;
-                case 7:
-                    Console.WriteLine("el mes es Julio");
-                    break;
-                case 8:
-                    Console.WriteLine("el mes es Agosto");
-                    break;
-                case 9:
-                    Console.WriteLine("el mes es Septiembre");
-                    break;
-                case 10:
-                    Console.WriteLine("el mes es Octubre");
-                    break;
-                case 11:
-                    Console.WriteLine("el mes es Noviembre");
-                    break;
-                case 12:
-                    Console.WriteLine("el mes es Diciembre");
-                    break;
-                default:
-                    Console.WriteLine("No es un mes");
-                    break;
-            }
+            //// pedir un numero numero del 1-12 y que aparezca el día del mes.
+            //int num1;
+            //Console.WriteLine("introducir numero mes: ");
+            //num1 = Convert.ToInt32(Console.ReadLine());
+            //switch (num1)
+            //{ case 1:
+            //        Console.WriteLine("el mes es Enero");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("el mes es Febrero");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("el mes es Marzo");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("el mes es Abril");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("el mes es Mayo");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("el mes es Junio");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("el mes es Julio");
+            //        break;
+            //    case 8:
+            //        Console.WriteLine("el mes es Agosto");
+            //        break;
+            //    case 9:
+            //        Console.WriteLine("el mes es Septiembre");
+            //        break;
+            //    case 10:
+            //        Console.WriteLine("el mes es Octubre");
+            //        break;
+            //    case 11:
+            //        Console.WriteLine("el mes es Noviembre");
+            //        break;
+            //    case 12:
+            //        Console.WriteLine("el mes es Diciembre");
+            //        break;
+            //    default:
+            //        Console.WriteLine("No es un mes");
+            //        break;
+            //}
+            //Console.ReadLine();
+
+            // Ejemplo notas
+            double nota;
+            Console.WriteLine("introducir valor examen: ");
+            nota = Convert.ToDouble(Console.ReadLine());
+            // si no se añade la condicón de >= 0 enla de suspenso, entraran los numeros negativos aquí y no en el else
+            if (nota < 5 && nota >= 0) { Console.WriteLine("Suspenso"); }
+            else if (nota >= 5 && nota < 6) { Console.WriteLine("Aprobado"); }
+            else if (nota >= 6 && nota < 7) { Console.WriteLine("Bien"); }
+            else if (nota >= 7 && nota < 9) { Console.WriteLine("Notable"); }
+            else if (nota >= 9 && nota == 10) { Console.WriteLine("Sobresaliente"); }
+            else { Console.WriteLine("Esto no es una nota, tramposo"); }
             Console.ReadLine();
+
+
+
+            //Ejercicios rápidos
+
+
         }
 
-        
+
 
         // Pedir el peso y altura, e indicar su Indice de Masa Corporal según tablas OMS
 
